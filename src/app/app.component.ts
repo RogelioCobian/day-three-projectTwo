0eb5002a946ae3b15f32f066b849e09f33531be8
Rogelio Cobian <rogeliocobian2@gmail.com>
2018-05-31 16:47:45 -0700

    started learning interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+interface Person {
+  firstName: string;
+  lastName: string;
+  customer: Person;
+
+  sayHi: string;
+
+}
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  ngOnInit() {
+
+  }
 }
